af124c85af2bd9452c0a413384d67ec8cbbe45bd
Roberto <avila.roberto84@gmail.com>
2020-02-29 13:08:41 -0800

    Added blog deletion and update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@
 #   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 # end
 Blogger::Application.routes.draw do
+  root to: 'articles#index'
   resources :articles
 end
